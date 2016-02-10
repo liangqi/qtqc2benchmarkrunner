6c03c2923480000d50b2501d216aaba1d7f0f2c4
Liang Qi <liang.qi@theqtcompany.com>
2016-02-10 07:33:30 +0100

    Added analyzer2() for line chart

--- qtqc2benchmarkrunner.py
+++ qtqc2benchmarkrunner.py
@@ -24,10 +24,7 @@ class QtQuickControls2BenchmarksRunner:
         self.modules = ['qtbase', 'qtxmlpatterns', 'qtsvg', 'qtdeclarative', 'qtgraphicaleffects', 'qtquickcontrols', 'qtquickcontrols2']
         self.sha1s = list()
         self.results = list()
-        self.r_tags = list()
-        self.r_sha1s = list()
-        self.r_values = list()
-        
+
     def initGitRepository(self):
         if os.path.exists(self.sourcedir):
             shutil.rmtree(self.sourcedir)
@@ -92,7 +89,7 @@ class QtQuickControls2BenchmarksRunner:
                 if os.path.exists(resultfile):
                     self.results.append(resultfile)
 
-    def checkResult(self, resultfile):
+    def checkResult(self, resultfile, tag, r_tags, r_values):
         xmldoc = minidom.parse(resultfile)
         tclist = xmldoc.getElementsByTagName('TestCase')
         for tc in tclist:
@@ -101,24 +98,24 @@ class QtQuickControls2BenchmarksRunner:
             for tf in tflist:
                 if 'initTestCase' in tf.attributes['name'].value or 'cleanupTestCase' in tf.attributes['name'].value:
                     continue
-                print('test: ' + tf.attributes['name'].value)
-                if tf.attributes['name'].value != 'controls':
+                #print('test: ' + tf.attributes['name'].value)
+                if tf.attributes['name'].value != tag:
                     continue
                 brlist = tf.getElementsByTagName('BenchmarkResult')
                 brsize = len(brlist)
-                print('brsize:' + str(brsize))
-                if len(self.r_tags) == 0:
-                    self.r_tags = range(brsize)
+                #print('brsize:' + str(brsize))
+                if len(r_tags) == 0:
+                    r_tags = range(brsize)
                 values = range(brsize)
                 index = 0
                 for br in brlist:
                     tag = br.attributes['tag'].value
                     value = br.attributes['value'].value
-                    if tag in self.r_tags:
-                        idx = self.r_tags.index(tag)
+                    if tag in r_tags:
+                        idx = r_tags.index(tag)
                         values[idx] = value
                     else:
-                        self.r_tags[index] = tag
+                        r_tags[index] = tag
                         values[index] = value
                     index = index + 1
                     # print('tag:' + br.attributes['tag'].value
@@ -126,7 +123,8 @@ class QtQuickControls2BenchmarksRunner:
                     #         + ', value:' + br.attributes['value'].value
                     #         + ', iterations:' + br.attributes['iterations'].value
                     #         )
-                self.r_values.append(values)
+                r_values.append(values)
+        return (r_tags, r_values)
         
     def getSha1(self):
         result = True
@@ -158,9 +156,9 @@ class QtQuickControls2BenchmarksRunner:
         for resultfile in self.results:
             shutil.copy(resultfile, targetdir)
                 
-    def getLatestSha1(self, modulepath, num=1):
+    def getLatestSha1(self, modulepath, num=1, fmt='%H'):
         os.chdir(modulepath)
-        p = subprocess.Popen(['git', 'log', '-' + str(num), '--pretty=format:%H'],
+        p = subprocess.Popen(['git', 'log', '-' + str(num), '--pretty=format:' + fmt, '--date=short'],
         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = p.communicate()
         return out
@@ -169,36 +167,101 @@ class QtQuickControls2BenchmarksRunner:
         print(','.join(self.modules))
         print('make -j9 module-' + ' module-'.join(self.modules))
 
-    def analyzer(self):
-        sha1s = self.getLatestSha1(self.sourcedir + '/qt5/qtquickcontrols2', 100)
-        print('sha1s are ' + sha1s)
+    def getSha1s(self, num=1, fmt='%H'):
+        rsha1s = list()
+        rdates = list()
+        sha1s = self.getLatestSha1(self.sourcedir + '/qt5/qtquickcontrols2', num, fmt)
+        #print('sha1s are ' + sha1s)
         sha1list = sha1s.splitlines(sha1s.count('\n'))
         for sha1 in reversed(sha1list):
             sha1 = sha1.rstrip()
             #print('sha1 is ' + sha1)
+            tmp = sha1.split(' ')
+            if len(tmp) < 2:
+                continue
+            resultfile = self.resultdir + '/' + tmp[0] + '/result-tst_creationtime.xml';
+            if os.path.exists(resultfile):
+                rsha1s.append(tmp[0])
+                rdates.append(tmp[1])
+        return (rsha1s, rdates)
+
+    def analyzer1(self, r_sha1s, tag):
+        r_tags = list()
+        r_values = list()
+        for sha1 in r_sha1s:
+            sha1 = sha1.rstrip()
+            #print('sha1 is ' + sha1)
             resultfile = self.resultdir + '/' + sha1 + '/result-tst_creationtime.xml';
             if os.path.exists(resultfile):
-                self.r_sha1s.append(sha1)
                 #print('found result file in ' + resultfile)
-                self.checkResult(resultfile)
-        result = 'var data = [\n'
-        result = result + '[ \'sha1s\', \'' + '\' , \''.join(self.r_sha1s) + '\' ],\n'
+                r_tags, r_values = self.checkResult(resultfile, tag, r_tags, r_values)
+        result = ' = [\n'
+        result = result + '[ \'sha1s\', \'' + '\' , \''.join(r_sha1s) + '\' ],\n'
         index = 0
-        vlen = len(self.r_values)
-        print('vlen:' + str(vlen))
-        for tag in self.r_tags:
+        vlen = len(r_values)
+        #print('vlen:' + str(vlen))
+        for tag in r_tags:
             if index != 0:
                 result = result + ', '
             result = result + '[ \'' + tag + '\', '
-            for vindex, values in enumerate(self.r_values, start=0):   # default is zero
+            for vindex, values in enumerate(r_values, start=0):   # default is zero
                 result = result + values[index]
-                print('vindex:' + str(vindex))
+                #print('vindex:' + str(vindex))
                 if vindex < vlen-1:
                     result = result + ', '
             result = result + ' ]\n'
             index = index + 1
         result = result + '];\n'
-        print(result)
+        #print(result)
+        return result
+
+    def analyzer2(self, r_sha1s, r_dates, tag):
+        r_tags = list()
+        r_values = list()
+        r_labels = list()
+        index = 0
+        for sha1 in r_sha1s:
+            sha1 = sha1.rstrip()
+            #print('sha1 is ' + sha1)
+            resultfile = self.resultdir + '/' + sha1 + '/result-tst_creationtime.xml';
+            if os.path.exists(resultfile):
+                #print('found result file in ' + resultfile)
+                r_tags, r_values = self.checkResult(resultfile, tag, r_tags, r_values)
+                r_labels.append(r_dates[index] + ' ' + sha1[0:7])
+                index = index + 1
+        result = ' = [\n'
+        result = result + '[ \'tags\', \'' + '\' , \''.join(r_tags) + '\' ],\n'
+        index = 0
+        for label in r_labels:
+            if index != 0:
+                result = result + ', '
+            result = result + '[ \'' + label + '\', ' + ','.join(r_values[index]) + ' ]\n'
+            index = index + 1
+        result = result + '];\n'
+        #print(result)
+        return result
+
+    def analyzerAll(self):
+        r_sha1s, r_dates = self.getSha1s(100, '%H %cd')
+        if not r_sha1s:
+            print('warning: analyzerAll() didn\'t get any sha1 back!')
+            return
+        print('sha1s: ' + ','.join(r_sha1s))
+        print('dates: ' + ','.join(r_dates))
+        t_sha1s = list()
+        t_sha1s.append(r_sha1s[len(r_sha1s)-1])
+        category = ['controls', 'material', 'universal', 'calendar']
+        for c in category:
+            s = 'var mydata' + self.analyzer1(t_sha1s, c)
+            sfn = self.resultdir + '/latest-' + c + '.js'
+            jsfile = open(sfn, "w")
+            jsfile.write(s)
+            jsfile.close()
+            s = 'var mydata' + self.analyzer2(r_sha1s, r_dates, c)
+            sfn = self.resultdir + '/' + c + '.js'
+            jsfile = open(sfn, "w")
+            jsfile.write(s)
+            jsfile.close()
                         
 if __name__ == "__main__":
         runner = QtQuickControls2BenchmarksRunner()
@@ -208,4 +271,4 @@ if __name__ == "__main__":
                runner.buildAndRunTest()
                print(','.join(runner.results))
                runner.saveResults()
-#        runner.analyzer()
+#        runner.analyzerAll()
